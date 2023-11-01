b3d16d3fb059056fceb3e7ae74c0e8ed8696f9f9
pjhcsols <110244523+pjhcsols@users.noreply.github.com>
2023-11-01 18:59:27 +0900

    Add files via upload
    init: project 초기화[박한솔]

---
+++ detection/src/test/java/com/abnormal/detection/DetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abnormal.detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
