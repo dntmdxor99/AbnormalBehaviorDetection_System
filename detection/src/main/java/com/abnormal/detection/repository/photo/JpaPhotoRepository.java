b5506e39092668794fe6d9acf79b201bc419b8ae
kimmokalover <kimmokalover@gmail.com>
2023-11-06 12:00:51 +0900

    feat : JpaPhotoRepository 구현 완료 [김승준]

--- detection/src/main/java/com/abnormal/detection/repository/photo/JpaPhotoRepository.java
+++ detection/src/main/java/com/abnormal/detection/repository/photo/JpaPhotoRepository.java
@@ -1,23 +1,34 @@
 package com.abnormal.detection.repository.photo;
 
+import com.abnormal.detection.domain.metadata.MetaData;
 import com.abnormal.detection.domain.photo.Photo;
+import jakarta.persistence.EntityManager;
 
 import java.util.List;
 
 public class JpaPhotoRepository implements PhotoRepository{
+
+    private final EntityManager em;
+
+    public JpaPhotoRepository(EntityManager em) {
+        this.em = em;
+    }
+
     @Override
     public Photo getPhotoById(Long photoId) {
-        return null;
+        return em.find(Photo.class, photoId);
     }
 
     @Override
     public List<Photo> getPhotosByVideoId(Long videoId) {
-        return null;
+        return em.createQuery("SELECT p FROM Photo p WHERE p.videoId = :videoId", Photo.class)
+                .setParameter("videoId", videoId).getResultList();
     }
 
     @Override
     public List<Photo> getPhotosByMetadataId(Long metaDataId) {
-        return null;
+        return em.createQuery("SELECT p FROM Photo p WHERE p.metaDataId = :metaDataId", Photo.class)
+                .setParameter("metaDataId", metaDataId).getResultList();
     }
 
     @Override
@@ -27,16 +38,30 @@ public class JpaPhotoRepository implements PhotoRepository{
 
     @Override
     public Photo savePhoto(Photo photo) {
-        return null;
+        em.persist(photo);
+        return photo;
     }
 
     @Override
     public Photo updatePhoto(Photo photo) {
-        return null;
+        Photo toUpdate = em.find(Photo.class, photo.getPhotoId());
+
+        if (toUpdate != null) {
+            toUpdate.setVideoId(photo.getVideoId());
+            toUpdate.setMetaDataId(photo.getMetaDataId());
+            toUpdate.setPathUrl(photo.getPathUrl());
+
+            em.merge(toUpdate);
+        }
+
+        return toUpdate;
     }
 
     @Override
     public void deletePhoto(Long photoId) {
-
+        Photo photo = em.find(Photo.class, photoId);
+        if (photo != null) {
+            em.remove(photo);
+        }
     }
 }
