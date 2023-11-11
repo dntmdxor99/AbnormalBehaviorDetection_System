3fba39e37e9830bee292a76887df28809053eb2c
김세아 <seakim@knu.ac.kr>
2023-11-12 00:05:05 +0900

    Chore: .gitignore, package.json

--- abnormal-front/src/components/Header.jsx
+++ abnormal-front/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 const Header = styled.section`
-  padding: 0 ${(props) => props.paddingValue}rem;
+  padding:1rem 2rem;
+  // 세로 1 가로 2
+
   background-color: white;
 `;
 export default Header;
