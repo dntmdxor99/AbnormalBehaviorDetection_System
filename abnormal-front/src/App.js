0dc54e713c89b0bb5ba8d7db3731eca3201533d2
김세아 <seakim@knu.ac.kr>
2023-11-12 02:51:03 +0900

    Design: 로그인, 로그아웃 버튼 추가

--- abnormal-front/src/App.js
+++ abnormal-front/src/App.js
@@ -11,24 +11,41 @@ import ResultPage from "./pages/ResultPage";
 import SearchPage from "./pages/SearchPage";
 import SignupPage from "./pages/SignupPage";
 import Header from "./components/Header";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Header>
-        <div className="menu">
-          <div className="menu-bar">
-            <Link to="/" className="menu-bar-item">7팀</Link>
-            <Link to='/abnormal' className="menu-bar-item">이상행동</Link>
-            <Link to='/cctv' className="menu-bar-item">CCTV</Link>
-            <Link to='/ask' className="menu-bar-item">문의하기</Link>
+        <Header>
+          <div className="menu">
+            <div className="menu-bar">
+              <Link to="/" className="menu-bar-item">
+                7팀
+              </Link>
+              <Link to="/abnormal" className="menu-bar-item">
+                이상행동
+              </Link>
+              <Link to="/cctv" className="menu-bar-item">
+                CCTV
+              </Link>
+              <Link to="/ask" className="menu-bar-item">
+                문의하기
+              </Link>
+              <Link>
+                <Link to="/login">
+                  <Button variant="outlined">로그인</Button>
+                </Link>
+              </Link>
+              <Link to="/signup">
+                <Button variant="contained">가입하기</Button>
+              </Link>
+            </div>
           </div>
-        </div>
-      </Header>
+        </Header>
         <Routes>
           <Route path="/" element={<MainPage />} />{" "}
-          {/* 홈화면 페이지 <Route path="/" element={<MainPage/>}/>*/}
           <Route path="/abnormal" element={<AbnormalBehaviorPage />} />
           <Route path="/cctv" element={<CctvPage />} />
           <Route path="/ask" element={<AskPage />} />
