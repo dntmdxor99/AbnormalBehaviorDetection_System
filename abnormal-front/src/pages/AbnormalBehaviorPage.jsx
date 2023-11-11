b9767305b12cc1241c93ffaec823d735d616447a
김세아 <seakim@knu.ac.kr>
2023-11-12 03:16:12 +0900

    Design: 이상행동 페이지 구성

---
+++ abnormal-front/src/pages/AbnormalBehaviorPage.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import React, {useEffect, useRef, useState} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import styled from 'styled-components';
+import PageLayout from '../components/PageLayout';
+
+const Frame = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+
+`
+const AbnormalBehaviorPage = () => {
+    return (
+        <PageLayout>
+            <Frame>
+              <div className='abnormal'>
+                <div className='abnormal-main'>
+                  <h1 className='abnormal-main-title'>
+                    이상행동
+                  </h1>
+                </div>
+              </div> 
+            </Frame>
+        </PageLayout>
+    )
+};
+
+export default AbnormalBehaviorPage;
