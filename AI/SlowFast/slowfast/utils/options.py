77b65473d0af45f3a5e6c021e231627b5d3e1ff5
dntmdxor99 <dntmdxor99@naver.com>
2023-12-03 04:46:21 +0900

    FIX : options.py 기능 세분화

--- AI/SlowFast/slowfast/utils/options.py
+++ AI/SlowFast/slowfast/utils/options.py
@@ -13,7 +13,9 @@ def yamlLoad(f):
 
 def parseOptions():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-opt', type=str, required=True, help='Path to option YAML file.')      # yaml file 경로를 입력,
+    parser.add_argument('-opt', type = str, required=True, help='Path to option YAML file.')      # yaml file 경로를 입력,
+    parser.add_argument('-gpu', type = int, help = 'Insert GPU Number')
+    parser.add_argument('-env', type = str, help = 'Insert your environment (Mac, Ubuntu)')
     args = parser.parse_args()
 
     opt = yamlLoad(args.opt)
@@ -21,10 +23,30 @@ def parseOptions():
     opt['middleDim'] = opt[backbone]['middleDim']
     opt['outputDim'] = opt[backbone]['outputDim']
     opt['stageBlocks'] = opt[backbone]['stageBlocks']
+
+    if args.gpu is None:
+        opt['numGpus'] = 0
+    else:
+        opt['numGpus'] = args.gpu
     
     if opt['numGpus'] >= 1:
         opt['device'] = 'cuda'
     else:
         opt['device'] = 'cpu'
+
+    if args.env is None:
+        opt['env'] = 'Ubuntu'
+    else:
+        opt['env'] = args.env
+
+    if opt['name'] == 'train':
+        opt['trainDataPath'] = opt[f'{opt["env"]}_trainDataPath']
+        opt['valDataPath'] = opt[f'{opt["env"]}_valDataPath']
+        opt['trainMetaInfoFile'] = opt[f'{opt["env"]}_trainMetaInfoFile']
+        opt['valMetaInfoFile'] = opt[f'{opt["env"]}_valMetaInfoFile']
+    else:
+        opt['testDataPath'] = opt[f'{opt["env"]}_testDataPath']
+        opt['pretrainedModelPath'] = opt[f'{opt["env"]}_pretrainedModelPath']
+
 
     return opt
