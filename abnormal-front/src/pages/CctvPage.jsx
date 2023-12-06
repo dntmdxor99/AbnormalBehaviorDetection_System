e40aaead3eeb7736db3ca1e3b696922cec070743
김세아 <seakim@knu.ac.kr>
2023-12-06 22:36:19 +0900

    Refactor : add frame and button

--- abnormal-front/src/pages/CctvPage.jsx
+++ abnormal-front/src/pages/CctvPage.jsx
@@ -6,11 +6,38 @@ import styled from "styled-components";
 import PageLayout from "../components/PageLayout";
 import API from "../utils/API";
 
-
 const Frame = styled.div`
   width: 100vw;
   height: 100vh;
 `;
+
+const Container = styled.div`
+  display: flex;
+  background-color: #ffffff;
+  padding: 35px 40px;
+  border-radius: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  margin: 20px 0;
+  width: 350px;
+  &:not(:first-child) {
+    margin-left: 50px;
+  }
+`;
+
+const Button = styled.div`
+  display: inline-block;
+  padding: 10px 15px;
+  font-size: 15px;
+  margin: 4px;
+  background-color: #3a3d92;
+  color: #ffffff;
+  transition: background-color 0.3s;
+  text-decoration: none;
+  border-radius: 5px;
+  // border: 2px solid #3a3d92;
+`;
+
 const CctvPage = () => {
   const [cctvData, setCctvData] = useState({
     cctvId: "",
@@ -19,7 +46,7 @@ const CctvPage = () => {
     latitude: "",
     longitude: "",
     is360Degree: "",
-    chsnnel: "",
+    channel: "",
     videoSize: "",
   });
   const [deleteId, setDeleteId] = useState("");
@@ -62,75 +89,86 @@ const CctvPage = () => {
         <div className="cctv">
           <div className="cctv-main">
             <h1 className="cctv-main-title">현재 등록된 CCTV</h1>
+            <div style={{ display: "flex" }}>
+              <Container>
+                <form onSubmit={onSubmit}>
+                  <h2>CCTV 등록</h2>
+                  <label>
+                    ID
+                    <input
+                      type="text"
+                      name="cctvId"
+                      value={""}
+                      onChange={onChange}
+                    />
+                  </label>
+                  <label>
+                    Name
+                    <input
+                      type="text"
+                      name="cctvName"
+                      value={cctvData.cctvName}
+                      onChange={onChange}
+                    />
+                  </label>
+                  <label>
+                    Location
+                    <input
+                      type="text"
+                      name="location"
+                      value={cctvData.location}
+                      onChange={onChange}
+                    />
+                  </label>
+                  <label>
+                    360Degree
+                    <input
+                      type="text"
+                      name="is360Degree"
+                      value={cctvData.is360Degree}
+                      onChange={onChange}
+                    />
+                  </label>
+                  <label>
+                    Channel
+                    <input
+                      type="text"
+                      name="channel"
+                      value={cctvData.channel}
+                      onChange={onChange}
+                    />
+                  </label>
+                  <label>
+                    VideoSize
+                    <input
+                      type="text"
+                      name="videoSize"
+                      value={cctvData.videoSize}
+                      onChange={onChange}
+                    />
+                  </label>
+                  <Button type="submit">등록</Button>
+                </form>
+              </Container>
+              <Container>
+                <form onSubmit={onDelete}>
+                  <h2>CCTV 삭제</h2>
+                  <label>
+                    ID:
+                    <input
+                      type="text"
+                      name="deleteId"
+                      value={deleteId}
+                      onChange={onIdChange}
+                    />
+                  </label>
+                  <Button type="button" onClick={onDelete}>
+                    삭제
+                  </Button>
+                </form>
+              </Container>
+            </div>
           </div>
-          <form onSubmit={onSubmit}>
-            <h2>CCTV 등록</h2>
-            <label>
-              ID:
-              <input type="text" name="cctvId" value={""} onChange={onChange} />
-            </label>
-            <label>
-              Name:
-              <input
-                type="text"
-                name="cctvName"
-                value={cctvData.cctvName}
-                onChange={onChange}
-              />
-            </label>
-            <label>
-              Location:
-              <input
-                type="text"
-                name="location"
-                value={cctvData.location}
-                onChange={onChange}
-              />
-            </label>
-            <label>
-              360Degree:
-              <input
-                type="text"
-                name="is360Degree"
-                value={cctvData.is360Degree}
-                onChange={onChange}
-              />
-            </label>
-            <label>
-              Protocol:
-              <input
-                type="text"
-                name="protocol"
-                value={cctvData.protocol}
-                onChange={onChange}
-              />
-            </label>
-            <label>
-              VideoSize:
-              <input
-                type="text"
-                name="videoSize"
-                value={cctvData.videoSize}
-                onChange={onChange}
-              />
-            </label>
-            <button type="submit">등록</button>
-          </form>
-          <form onSubmit={onDelete}>
-            <h2>CCTV 삭제</h2>
-            <label>
-              ID:
-              <input
-                type="text"
-                name="deleteId"
-                value={deleteId}
-                onChange={onIdChange}
-              />
-            </label>
-            <button type="button" onClick={onDelete}>
-              삭제
-            </button>
-          </form>
         </div>
       </Frame>
     </PageLayout>
