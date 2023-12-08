a3bfc348824f10620656264d83e4760aae579a08
MINJUN <kmjj5275@naver.com>
2023-12-09 00:17:37 +0900

    Chore: 변경사항 커밋

--- abnormal-front/src/utils/API.js
+++ abnormal-front/src/utils/API.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-const BASE_URL = "http://172.20.5.89:8080";
-=======
 const BASE_URL = "http://172.20.5.197:8080";
->>>>>>> 4895692b4d711043d9334eb95c809d5040ebe841
 
 const API = axios.create({
     baseURL: BASE_URL,
